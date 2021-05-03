11407c67477c93e77610de7ed3a6dc6e24510928
flexy2ky <feel_me2ky@yahoo.com>
2021-05-03 13:18:22 +0100

    Customizations

--- erpnext/setup/install.py
+++ erpnext/setup/install.py
@@ -13,7 +13,7 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 from erpnext.setup.default_energy_point_rules import get_default_energy_point_rules
 
 default_mail_footer = """<div style="padding: 7px; text-align: right; color: #888"><small>Sent via
-	<a style="color: #888" href="http://erpnext.org">ERPNext</a></div>"""
+	<a style="color: #888" href="http://flexcomsystems.net">Flexcom ERP</a></div>"""
 
 
 def after_install():
@@ -124,19 +124,19 @@ def add_standard_navbar_items():
 		{
 			'item_label': 'Documentation',
 			'item_type': 'Route',
-			'route': 'https://erpnext.com/docs/user/manual',
+			'route': 'https://flexcomsystems.net',
 			'is_standard': 1
 		},
 		{
 			'item_label': 'User Forum',
 			'item_type': 'Route',
-			'route': 'https://discuss.erpnext.com',
+			'route': 'https://flexcomsystems.net/forums',
 			'is_standard': 1
 		},
 		{
 			'item_label': 'Report an Issue',
 			'item_type': 'Route',
-			'route': 'https://github.com/frappe/erpnext/issues',
+			'route': 'https://flexcomsystems.net/issues',
 			'is_standard': 1
 		}
 	]
